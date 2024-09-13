6ee200024aaf8d5a79e28c9281d7766386284ac1
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2024-09-12 23:54:30 -0300

    first commit

---
+++ TFIGrupo3/src/test/java/TFIGrupo3/TFIGrupo3/TfiGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package TFIGrupo3.TFIGrupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfiGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
