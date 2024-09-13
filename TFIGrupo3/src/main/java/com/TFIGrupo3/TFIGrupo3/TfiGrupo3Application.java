7b8e205494314621fc6e70293d1674ec25b572f2
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2024-09-13 14:38:25 -0300

    Se cambio el proyecto a Maven

---
+++ TFIGrupo3/src/main/java/com/TFIGrupo3/TFIGrupo3/TfiGrupo3Application.java
@@ -0,0 +1,13 @@
+package com.TFIGrupo3.TFIGrupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfiGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfiGrupo3Application.class, args);
+	}
+
+}
